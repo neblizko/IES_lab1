8789d16b3fa7aa29144a46e5e310895add72dc3e
Daria Diakonenko <daria.diakonenko@gmail.com>
2021-02-17 13:18:56 +0200

    additional files

---
+++ lab 1.1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace lab1._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.d_value = new System.Windows.Forms.Label();
+            this.m_value = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Interval = 100D;
+            chartArea1.AxisX.IsMarksNextToAxis = false;
+            chartArea1.AxisX.Maximum = 65D;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.Maximum = 4D;
+            chartArea1.AxisY.Minimum = -4D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "Signal";
+            series1.YValuesPerPoint = 2;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 426);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 476);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "M =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 534);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "D =";
+            // 
+            // d_value
+            // 
+            this.d_value.AutoSize = true;
+            this.d_value.Location = new System.Drawing.Point(162, 534);
+            this.d_value.Name = "d_value";
+            this.d_value.Size = new System.Drawing.Size(31, 17);
+            this.d_value.TabIndex = 1;
+            this.d_value.Text = "N/A";
+            // 
+            // m_value
+            // 
+            this.m_value.AutoSize = true;
+            this.m_value.Location = new System.Drawing.Point(162, 476);
+            this.m_value.Name = "m_value";
+            this.m_value.Size = new System.Drawing.Size(31, 17);
+            this.m_value.TabIndex = 1;
+            this.m_value.Text = "N/A";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(812, 602);
+            this.Controls.Add(this.d_value);
+            this.Controls.Add(this.m_value);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Chart";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label d_value;
+        private System.Windows.Forms.Label m_value;
+    }
+}
+
---
+++ lab 1.1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+            double[] s = Program.generateSignal(Program.CALLS_NUMBER, Program.HARMONICS_NUMBER, Program.BORDER_FREQUENCY);
+            double M = Program.calculateM(s);
+            double D = Program.calculateD(M, s);
+            Console.WriteLine("Chart was drawn");
+            fillForm(s, M, D);
+        }
+
+        public void fillForm(double[] s, double M, double D)
+        {
+            Console.WriteLine(s.Length.ToString());
+            for (int i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i].ToString());
+
+                chart1.Series["Signal"].Points.AddXY(i, s[i]);
+            }
+            m_value.Text = M.ToString();
+            d_value.Text = D.ToString();
+
+        }
+    }
+}
