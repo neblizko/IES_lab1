fbeae29e305f0e9105e96a80350962a9852677bb
Daria Diakonenko <daria.diakonenko@gmail.com>
2021-05-09 16:06:04 +0300

    add report

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -35,6 +35,13 @@
             this.percButton = new System.Windows.Forms.Button();
             this.percResult = new System.Windows.Forms.Label();
             this.percIterations = new System.Windows.Forms.TextBox();
+            this.geneticA = new System.Windows.Forms.TextBox();
+            this.geneticB = new System.Windows.Forms.TextBox();
+            this.geneticC = new System.Windows.Forms.TextBox();
+            this.geneticD = new System.Windows.Forms.TextBox();
+            this.geneticY = new System.Windows.Forms.TextBox();
+            this.geneticCount = new System.Windows.Forms.Button();
+            this.geneticRes = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // fermButton
@@ -95,17 +102,78 @@
             this.percIterations.Size = new System.Drawing.Size(100, 22);
             this.percIterations.TabIndex = 3;
             // 
+            // geneticA
+            // 
+            this.geneticA.Location = new System.Drawing.Point(355, 13);
+            this.geneticA.Name = "geneticA";
+            this.geneticA.Size = new System.Drawing.Size(100, 22);
+            this.geneticA.TabIndex = 3;
+            // 
+            // geneticB
+            // 
+            this.geneticB.Location = new System.Drawing.Point(355, 41);
+            this.geneticB.Name = "geneticB";
+            this.geneticB.Size = new System.Drawing.Size(100, 22);
+            this.geneticB.TabIndex = 3;
+            // 
+            // geneticC
+            // 
+            this.geneticC.Location = new System.Drawing.Point(355, 67);
+            this.geneticC.Name = "geneticC";
+            this.geneticC.Size = new System.Drawing.Size(100, 22);
+            this.geneticC.TabIndex = 3;
+            // 
+            // geneticD
+            // 
+            this.geneticD.Location = new System.Drawing.Point(355, 95);
+            this.geneticD.Name = "geneticD";
+            this.geneticD.Size = new System.Drawing.Size(100, 22);
+            this.geneticD.TabIndex = 3;
+            // 
+            // geneticY
+            // 
+            this.geneticY.Location = new System.Drawing.Point(355, 123);
+            this.geneticY.Name = "geneticY";
+            this.geneticY.Size = new System.Drawing.Size(100, 22);
+            this.geneticY.TabIndex = 3;
+            // 
+            // geneticCount
+            // 
+            this.geneticCount.Location = new System.Drawing.Point(367, 166);
+            this.geneticCount.Name = "geneticCount";
+            this.geneticCount.Size = new System.Drawing.Size(75, 23);
+            this.geneticCount.TabIndex = 0;
+            this.geneticCount.Text = "Count";
+            this.geneticCount.UseVisualStyleBackColor = true;
+            this.geneticCount.Click += new System.EventHandler(this.geneticCount_Click);
+            // 
+            // geneticRes
+            // 
+            this.geneticRes.AutoSize = true;
+            this.geneticRes.Location = new System.Drawing.Point(364, 201);
+            this.geneticRes.Name = "geneticRes";
+            this.geneticRes.Size = new System.Drawing.Size(46, 17);
+            this.geneticRes.TabIndex = 5;
+            this.geneticRes.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.geneticRes);
             this.Controls.Add(this.percResult);
             this.Controls.Add(this.percButton);
             this.Controls.Add(this.percIterations);
+            this.Controls.Add(this.geneticY);
+            this.Controls.Add(this.geneticD);
+            this.Controls.Add(this.geneticC);
+            this.Controls.Add(this.geneticB);
+            this.Controls.Add(this.geneticA);
             this.Controls.Add(this.percSigma);
             this.Controls.Add(this.fermResult);
             this.Controls.Add(this.fermInput);
+            this.Controls.Add(this.geneticCount);
             this.Controls.Add(this.fermButton);
             this.Name = "Form1";
             this.Text = "Lab 3";
@@ -123,6 +191,13 @@
         private System.Windows.Forms.Button percButton;
         private System.Windows.Forms.Label percResult;
         private System.Windows.Forms.TextBox percIterations;
+        private System.Windows.Forms.TextBox geneticA;
+        private System.Windows.Forms.TextBox geneticB;
+        private System.Windows.Forms.TextBox geneticC;
+        private System.Windows.Forms.TextBox geneticD;
+        private System.Windows.Forms.TextBox geneticY;
+        private System.Windows.Forms.Button geneticCount;
+        private System.Windows.Forms.Label geneticRes;
     }
 }
 
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -46,7 +46,6 @@ namespace lab3
             ulong[] arr = FermatFactor(num);
             fermResult.Text = string.Concat(arr[0].ToString(), ", ", arr[1].ToString());
         }
-        ///************************************************///
 
         ///*******************PERCEPTRON*******************///
         private void percButton_Click(object sender, EventArgs e)
@@ -102,6 +101,151 @@ namespace lab3
                 i += 1;
             }
 
+        }
+
+        ///*******************GENETIC*******************///
+        private int population = 50;
+        private int max = 5;
+        private long time = 50;
+        private int[][] gens = new int[50][];
+        
+        int[] deltas = new int[50];
+        double[] survival = new double[50];
+        private int calculateDeltas (int a, int b, int c, int d, int y)
+        {
+            for (int i = 1; i < population+1; ++i)
+            {
+                //Console.WriteLine();
+                //Console.WriteLine("[{0}]", string.Join(", ", gens[0])); 
+                int result = a * gens[i-1][0] + b * gens[i-1][1] + c * gens[i-1][2] + d * gens[i-1][3];
+                deltas[i-1] = Math.Abs(result - y);
+                if (deltas[i-1] == 0) return i;
+            }
+            return -1;
+        }
+
+        private int getParent()
+        {
+            int[] chances = new int[50];
+
+            for (int i = 0; i < population; i++)
+            {
+                chances[i] = (new Random().Next(1, max)) * deltas[i];
+            }
+
+           var gen = -Double.PositiveInfinity;
+           int parent = 0;
+
+            for (int i = 0; i < population; i++)
+            {
+                if (gen < chances[i])
+                {
+                    gen = chances[i];
+                    parent = i;
+                }
+            }
+            
+            return parent;
+        }
+        //private int GetTime()
+        //{
+
+        //    DateTime OLDtime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        //    TimeSpan diff = date.ToUniversalTime() - OLDtime;
+        //    return Math.Floor(diff.TotalSeconds);
+        //}
+        private void geneticCount_Click(object sender, EventArgs e)
+        {
+            string res;
+            int a = int.Parse(geneticA.Text);
+            int b = int.Parse(geneticB.Text);
+            int c = int.Parse(geneticC.Text);
+            int d = int.Parse(geneticD.Text);
+            int y = int.Parse(geneticY.Text);
+            long start = DateTime.Now.Millisecond;
+
+            for (int i = 0; i < population; i++)
+            {
+                gens[i] = new int[4];
+                gens[i][0] = (new Random().Next(1, max));
+                gens[i][1] = (new Random().Next(1, max));
+                gens[i][2] = (new Random().Next(1, max));
+                gens[i][3] = (new Random().Next(1, max));
+            }
+
+            int index = -1;
+            for (int i = 0; i < population; ++i)
+            {
+
+                int result = a * gens[i][0] + b * gens[i][1] + c * gens[i][2] + d * gens[i][3];
+                deltas[i] = Math.Abs(result - y);
+                if (deltas[i] == 0) index = i;
+            }
+
+            if (index != -1)
+            {
+                geneticRes.Text = "x1 = " + gens[index][0].ToString() + " x2 = " + gens[index][1] + " x3 = " + gens[index][2] + " x4 = " + gens[index][3] + " delta = " + deltas[index];
+            }
+
+            long end = DateTime.Now.Millisecond;
+
+            while (end - start < time)
+            {
+                Console.WriteLine(end-start + " " + time);
+                double allSurvival = 0;
+
+                for (int i = 0; i < population; i++)
+                {
+                    allSurvival += 1 / deltas[i];
+                }
+
+                for (int i = 0; i < population; i++)
+                {
+                    survival[i] = (1 / deltas[i]) / allSurvival;
+                }
+
+                int father = getParent();
+                int mother = getParent();
+
+                int[][] children = new int[50][];
+
+                for(int i = 0; i < population; i++)
+                {
+                    children[i] = new int[4];
+                    children[i][0] = gens[father][0];
+                    children[i][1] = gens[father][1];
+                    children[i][2] = gens[father][2];
+                    children[i][3] = gens[father][3];
+                }
+                gens = children;
+                index = -1;
+                for (int i = 0; i < population; ++i)
+            {
+
+                int result = a * gens[i][0] + b * gens[i][1] + c * gens[i][2] + d * gens[i][3];
+                deltas[i] = Math.Abs(result - y);
+                if (deltas[i] == 0) index = i;
+            }
+
+                if (index != -1)
+                {
+                    geneticRes.Text = "x1 = " + children[index][0] + " x2 = " + children[index][1] + " x3 = " + children[index][2] + " x4 = " + children[index][3] + " delta = " + deltas[index];
+                }
+
+                end = DateTime.Now.Millisecond;
+            }
+
+            var smallDelta = Double.PositiveInfinity;
+            int deltaIndex = 0;
+            for(int i = 0; i < population; i++)
+            {
+                if (deltas[i] < smallDelta)
+                {
+                    smallDelta = deltas[i];
+                    deltaIndex = i;
+                }
+            }
+            geneticRes.Text = "x1 = " + gens[deltaIndex][0].ToString() + " x2 = " + gens[deltaIndex][1] + " x3 = " + gens[deltaIndex][2] + " x4 = " + gens[deltaIndex][3] + " delta = " + deltas[deltaIndex];
         }
     }
 }
